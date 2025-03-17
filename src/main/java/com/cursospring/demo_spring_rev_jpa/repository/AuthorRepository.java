0cf49ff4fb235d5b91ae238fd6d54bc1abc1e88a
Clessio <clessio.silva1989@gmail.com>
2025-03-17 07:56:22 -0300

    create method in AuthorRepository

--- src/main/java/com/cursospring/demo_spring_rev_jpa/repository/AuthorRepository.java
+++ src/main/java/com/cursospring/demo_spring_rev_jpa/repository/AuthorRepository.java
@@ -1,9 +1,16 @@
 package com.cursospring.demo_spring_rev_jpa.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.cursospring.demo_spring_rev_jpa.entities.Author;
 
-public interface AuthorRepository extends JpaRepository<Author, Long>{
-	
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+	@Query("select a from Author a where a.infoAuthor.cargo like :cargo order by a.nome asc")
+	List<Author> findByCargo(@Param("cargo") String cargo);
+
 }
