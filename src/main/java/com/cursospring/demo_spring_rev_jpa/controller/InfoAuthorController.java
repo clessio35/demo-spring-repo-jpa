51507800e022ce6da7f13beef234d9ffc3a7276e
Clessio <clessio.silva1989@gmail.com>
2025-03-20 08:34:47 -0300

    Add new method in InfoAuthor findById, and RequestMapping (info)

--- src/main/java/com/cursospring/demo_spring_rev_jpa/controller/InfoAuthorController.java
+++ src/main/java/com/cursospring/demo_spring_rev_jpa/controller/InfoAuthorController.java
@@ -1,9 +1,12 @@
 package com.cursospring.demo_spring_rev_jpa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cursospring.demo_spring_rev_jpa.entities.InfoAuthor;
 import com.cursospring.demo_spring_rev_jpa.service.InfoAuthorService;
 
 @RestController
@@ -12,4 +15,9 @@ public class InfoAuthorController {
 
 	@Autowired
 	private InfoAuthorService service;
+	
+	@GetMapping("{id}")
+	public InfoAuthor getById(@PathVariable Long id) {
+		return service.findById(id);
+	}
 }
--- src/main/java/com/cursospring/demo_spring_rev_jpa/service/InfoAuthorService.java
+++ src/main/java/com/cursospring/demo_spring_rev_jpa/service/InfoAuthorService.java
@@ -1,9 +1,11 @@
 package com.cursospring.demo_spring_rev_jpa.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cursospring.demo_spring_rev_jpa.entities.InfoAuthor;
 import com.cursospring.demo_spring_rev_jpa.repository.InfoAuthorRepository;
 
 @Service
@@ -13,4 +15,9 @@ public class InfoAuthorService {
 	@Autowired
 	private InfoAuthorRepository repository;
 	
+	public InfoAuthor findById(Long id) {
+		InfoAuthor info = new InfoAuthor();
+		info.setId(id);
+		return this.repository.findOne(Example.of(info)).orElseGet(InfoAuthor::new);
+	}
 }
