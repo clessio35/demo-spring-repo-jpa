6c939c7b18243e2755282d308cfc6a7442ae60cd
Clessio <clessio.silva1989@gmail.com>
2025-03-21 07:59:24 -0300

    findAll methods by cargo with ExampleMatcher

--- src/main/java/com/cursospring/demo_spring_rev_jpa/controller/InfoAuthorController.java
+++ src/main/java/com/cursospring/demo_spring_rev_jpa/controller/InfoAuthorController.java
@@ -1,5 +1,7 @@
 package com.cursospring.demo_spring_rev_jpa.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,5 +21,10 @@ public class InfoAuthorController {
 	@GetMapping("{id}")
 	public InfoAuthor getById(@PathVariable Long id) {
 		return service.findById(id);
+	}
+	
+	@GetMapping("cargo/{cargo}")
+	public List<InfoAuthor> getContainsCargo(@PathVariable String cargo) {
+		return service.findAllContainsCargo(cargo);
 	}
 }
--- src/main/java/com/cursospring/demo_spring_rev_jpa/service/InfoAuthorService.java
+++ src/main/java/com/cursospring/demo_spring_rev_jpa/service/InfoAuthorService.java
@@ -1,7 +1,10 @@
 package com.cursospring.demo_spring_rev_jpa.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,5 +22,13 @@ public class InfoAuthorService {
 		InfoAuthor info = new InfoAuthor();
 		info.setId(id);
 		return this.repository.findOne(Example.of(info)).orElseGet(InfoAuthor::new);
+	}
+	
+	public List<InfoAuthor> findAllContainsCargo(String cargo) {
+		InfoAuthor info = new InfoAuthor();
+		info.setCargo(cargo);
+		ExampleMatcher matcher = ExampleMatcher.matching()
+				.withMatcher("cargo", ExampleMatcher.GenericPropertyMatchers.contains());
+		return this.repository.findAll(Example.of(info, matcher));
 	}
 }
